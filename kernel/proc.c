442282e1dce32876881c609716aaa6deed6bfe15
Arie1906 <nttislinked@gmail.com>
2025-04-26 03:55:51 +0000

    oof

--- kernel/proc.c
+++ kernel/proc.c
@@ -298,6 +298,7 @@ fork(void)
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
+  np->flagpgtbl = p->flagpgtbl;
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
